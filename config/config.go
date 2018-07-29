77fa42b1d068f0757687d0d48e8189dad0ee89b1
Oshank Kumar <okumar@redhat.com>
2018-07-29 12:25:23 +0530

    added env pairs

--- config/config.go
+++ config/config.go
@@ -5,8 +5,25 @@ import (
 	"github.com/spf13/viper"
 )
 
+var (
+	envMap = map[string]string{
+		"env" : "ENV",
+		"services.nlu.addr" : "NLU",
+		"services.auth.addr": "AUTH",
+		"log.level" : "LOG_LEVEL",
+		"http.port" : "HTTP_PORT",
+	}
+)
 func init() {
 	viper.SetConfigFile(utils.GetConfFile())
 	viper.ReadInConfig()
-	viper.BindEnv()
+	viper.BindEnv(getEnvPairs()...)
+}
+
+func getEnvPairs()([]string){
+	var envpair []string
+	for key,val := range envMap{
+		envpair = append(envpair,key,val)
+	}
+	return envpair
 }
